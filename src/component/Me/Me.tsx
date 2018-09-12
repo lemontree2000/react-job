371e2b41689c81841be33b9888fe836181c42182
lemontree2000 <gonelove.guo@gmail.com>
2018-09-12 23:23:17 +0800

    update

--- src/component/Me/Me.tsx
+++ src/component/Me/Me.tsx
@@ -1,25 +1,55 @@
 import React from 'react'
 import { connect } from 'react-redux';
-import { Result } from 'antd-mobile';
+import { Result, List, WhiteSpace, Modal } from 'antd-mobile';
+// import browserCookies from 'browser-cookies';
 
+const { alert } = Modal;
 @(connect(
-    (state: any) =>  state.User ,
+    (state: any) => state.User,
     {}
 ) as any)
 
 class Me extends React.Component<any, any> {
     constructor(props: any) {
         super(props);
+        this.logout = this.logout.bind(this);
+    }
+    public logout() {
+        console.log(this);
+
+        // browserCookies.erase('user');
     }
     public render() {
         console.log(this.props);
-        const { avatar, user } = this.props;
+        const { avatar, user, type, company, title, desc, money } = this.props;
+        const { Item } = List;
+        const { Brief } = Item;
         return user ? <div>
             <Result
-                img={<img width={55} style={{borderRadius: '50%'}} src={require(`../../assets/image/${avatar}.png`)} />}
+                img={<img width={55} style={{ borderRadius: '50%' }} src={require(`../../assets/image/${avatar}.png`)} />}
                 title={user}
+                message={type === 'boss' ? company : ''}
             />
-            <p>用户中心</p>
+            <List renderHeader={() => '简介'}>
+                <Item
+                    multipleLine={true}
+                >
+                    {title}
+                    {desc.split('\n').map((v: any) => (<Brief key={v}>{v}</Brief>))}
+                    {money ? <Brief>薪资：{money}</Brief> : ''}
+                </Item>
+            </List>
+            <WhiteSpace />
+            <List >
+                <Item onClick={()=>{
+                            alert('退出登录', '是否确认退出登录？？', [
+                                { text: 'Cancel', onPress: () => console.log('cancel') },
+                                { text: 'Ok', onPress: () => this.logout() },
+                            ])
+                        }}>
+                    退出登录
+                </Item>
+            </List>
         </div> : null
     }
 }
